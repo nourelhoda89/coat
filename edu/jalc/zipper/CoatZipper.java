e3203884bdf988521db4322811671268a1ec5b0d
nourelhoda89 <nfarhat6558@volmail.jalc.edu>
2017-02-16 10:50:59 -0600

    Update CoatZipper.java

--- edu/jalc/zipper/CoatZipper.java
+++ edu/jalc/zipper/CoatZipper.java
@@ -1,1 +1,34 @@
+package edu.jalc.zipper;
+public class CoatZipper {
 
+	private boolean withZipper;
+	private final double lenght;
+
+	private CoatZipper(){
+		this.withZipper = false;
+	   this.lenght = 0;
+	}
+
+	public CoatZipper( double lenght){
+
+		this.lenght = lenght;
+		this.withZipper = false;
+	}
+
+	public double getLenght(){ return this.lenght; }
+	public boolean isItWithZipper(){ return this.withZipper; }
+
+	public CoatZipper included(){
+		this.setWithZipper(true);
+		return this;
+	}
+
+	public CoatZipper notIncluded(){
+		this.setWithZipper(false);
+		return this;
+	}
+
+	private void setWithZipper(boolean withZipper){
+		this.withZipper = withZipper;}
+
+}
